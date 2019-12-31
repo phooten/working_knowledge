a847a980bf41aafece7fdaf7b4640747a5065306
phooten <parkerhooten@gmail.com>
2019-12-30 22:35:51 -0600

    Problem 1: min/max finding

--- testing_1.py
+++ testing_1.py
@@ -1,1 +1,47 @@
-#This is a testing file to help figure out git
+# problem 1
+# start date: [ december 30 2019 ]
+# source:
+#		https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/min-max-8/
+
+
+# PROJECT NAME
+# PROJECT DESCRIPTION
+# BUNCH O'FILES
+
+#	PASTE PROBLEM STATEMENT LATER
+
+# HELPFUL NOTES:
+# - "b = 12L" - add an "L" or "l" after the number to make variable as long integer
+
+#GLOBAL VARIABLE
+min = 0
+max = 0
+
+#FUNCTIONS:
+def calc(Narray, N):
+	# option 1:
+	global min
+	global max
+	for i in range(0, N-1):
+		min = min + Narray[i]
+		#print(min, "\n")
+	
+	for i in range(1, N):
+		max = max + Narray[i]
+		#print(max, "\n")
+
+# all print statements would normally be deleted
+print("Input value of N: ")
+N = int(input())
+
+print("Input N list (seperate with spaces): ")
+ 
+Narray = list(map(int, input().split()))
+
+calc(Narray, N)
+print(min, " ", max)
+
+# print("N: ", N, " Narray: ", Narray)
+# print(min, " ", max)
+
+
