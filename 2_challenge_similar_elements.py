08f48cec38b89a8147fd5c2c83a95f7bbb1eb7a6
phooten <parkerhooten@gmail.com>
2020-01-04 21:37:50 -0600

    almost complete

--- 2_challenge_similar_elements.py
+++ 2_challenge_similar_elements.py
@@ -1,19 +1,26 @@
-# Code by Parker Hooten
+# Status:
+#		incomplete as of [1/4/2020]
+#		1 forseeable issue
 # Purpose: 
-#	Complete a challenge from "hackerearth.com" relating to arrays and finding similar
-#	characteristics.
+#		Complete a challenge from "hackerearth.com" relating to arrays and finding similar
+#		characteristics.
 # Problem Source: 
-# 	-> https://www.hackerearth.com/practice/data-structures/arrays/1-d/practice-problems/algorithm/pairs-having-similar-element-eed098aa/description/
+# 		-> https://www.hackerearth.com/practice/data-structures/arrays
+#		   /1-d/practice-problems/algorithm/pairs-having-similar-element-eed098aa/description/
 
 
 # Log
 #--------------------------------------------------------------------------------------
 # [12/31/2019]
-# - Most of the code is completed with ease. Ran into a transitivity matching issue. I can't 
-#	seem to complete this without a massive chunk of code.
+#     - Most of the code is completed with ease. Ran into a transitivity matching issue. I can't 
+#		seem to complete this without a massive chunk of code.
 # [1/1/2020] 
-# - still having issues with the transitivity portion. I forsee an issue where the number of 
-#	of matches will out number what I have alloted for the array
+#     - still having issues with the transitivity portion. I forsee an issue where the number of 
+#		of matches will out number what I have alloted for the array
+# [1/4/2020]
+#     - Found another way to add dynamically add to matches list AND nest lists, helping with keep
+#		track of pairs. Transitiviy checking doesn't account for the first element of the 
+#			first element in pairs() i.e. pairs[0][0] 
 #--------------------------------------------------------------------------------------
 
 
@@ -24,43 +31,49 @@ def calc(arr, n):
 	matches = 0							# total similarity matches
 	difference = 0						# used to determine if similar
 	track = 0							# tracks element position in "trans[]"
-	trans = [0 for h in range(0, n)]	# holds match pairs
-
+	pairs = []
+	trans = []
+	
 	# compares every number in the array using "Selective sort"
 	for i in range(0, n):
 		for j in range(i+1, n):	
 
-			# determines if numbers are similar
+			# checks if numbers are similar
 			difference = arr[i] - arr[j]
 			if difference == 1 or difference == -1:
 				matches = matches + 1
-
-				# adds the matched pair to an array
-				trans[track] = i
-				track += 1
-				trans[track] = j
-				track += 1
-				print("trans = ", trans)
-				print("Match with ", arr[i], " and ", arr[j], " total: ", matches, "\n")  
+				pairs.append((i, j))
+				# print("length: ", len(pairs), " ", pairs)
+				
+	# print("unsorted: ", pairs)
+	pairs.sort()
+	# print("sorted: ", pairs)
 	
-	"""	
-	# attempt at finding duplicates to find transitivity
-	for j in range(0, len(trans), 2):
-		for k in range(j, len(trans), 2):
-			if trans[j] == trans [k]:
-				matches += 1
-	"""
+	# tracks matched pairs, checks for transitivity
+	for k in range(0, len(pairs)-1):	
+		if pairs[k][1] == pairs[k+1][1]:			# Possibly skips checking "pairs[0][0]", see log
+			matches += 1
+			trans.append(pairs[k][1])
+			trans.append(pairs[k+1][1])
+		
+		if pairs[k][1] == pairs[k+1][0]:
+			matches += 1
+			trans.append(pairs[k][1])
+			trans.append(pairs[k+1][0])
+		# print(trans)
 	return matches
 
 
-# main code
-print("Number of elements in the array: ")
+# MAIN
+#----------------------------------------------------
+
+# print("Number of elements in the array: ")
 n = int(input())
 
-print("location of \"i\" element: ")
+# print("location of \"i\" element: ")
 arrayInput = list(map(int, input().split()))
 
 pairs = calc(arrayInput, n)
-print("done: ", pairs)
+print(pairs)
 
-
+#----------------------------------------------------
