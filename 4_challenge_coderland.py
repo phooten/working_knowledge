8a006f6981db3dbf8dd1bac1437ea996445df598
phooten <parkerhooten@gmail.com>
2020-01-06 02:09:52 -0600

    5%

---
+++ 4_challenge_coderland.py
@@ -0,0 +1,46 @@
+# Purpose: 
+# Problem Source: 
+
+
+# Log
+#--------------------------------------------------------------------------------------
+# [1/6/2020]
+#     - Finding a way to use the ternary search algorithem is quite difficult
+#--------------------------------------------------------------------------------------
+
+
+
+
+# Notes:
+# 1. HAVE to buy from the first stations
+# 2. 
+
+
+def search(l, r, arr, x):
+
+	return ans
+
+# MAIN
+#----------------------------------------------------
+t = int(input())
+for h in range(0, t):
+	n = int(input())
+	
+	c = list(map(int, input().split()))
+	l = list(map(int, input().split()))
+	
+	
+	""" attempt 1-------------------------------------
+	
+	combined = []
+	for i in range(0, n):
+		combined.append((c[i], l[i], i))
+	duplicate = combined
+	duplicate.sort()
+	least = duplicated[0][0]
+	---------------------------------------------
+	"""
+	# search(l, r, arr, least)
+
+	print(combined)
+#----------------------------------------------------
