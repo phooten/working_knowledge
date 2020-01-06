1d46eed593edcc43f682730174de8ce400b19fc7
phooten <parkerhooten@gmail.com>
2020-01-06 00:45:37 -0600

    30%

--- 3_challenge_perfect_pair.py
+++ 3_challenge_perfect_pair.py
@@ -7,8 +7,10 @@
 # Log
 #--------------------------------------------------------------------------------------
 # [1/2/2019]
-# - Most of the code is done. I have not incorporated code to check if (Ai, Aj) and (Aj, Ai)
-#   are both included or not. 
+# 		- Most of the code is done. I have not incorporated code to check if (Ai, Aj) and (Aj, Ai)
+#   	  are both included or not. 
+# [1/5/2019]
+# 		- I think I need to learn more about hashing then come back to this.   
 #--------------------------------------------------------------------------------------
 
  
@@ -26,8 +28,7 @@ def hash(N, AN):
 		for j in range (i+1, N):
 			# checks for perfect root or cube
 			numbers = (AN[i], AN[j])
-			result = math.sqrt(sum(numbers))
-			if (result).is_integer():
+			if (sum(numbers) ** (1/2)).is_integer():
 				total += 1
 			elif (sum(numbers) ** (1/3)).is_integer():
 				total += 1
@@ -38,13 +39,28 @@ def hash(N, AN):
 T = int(input())							# number of test cases
 
 for x in range(0, T):
+	"""
+	# sets up the hash function
 	N = int(input())						# number of elements in array
+	AN = [0] * 1001
+	for i in input().split():
+		AN[int(i)] += 1  						# 1000 possible inputs 
+		print(AN)
+
+	B = [0] * 2001
+	i = 0
+	while(i * i < 2000):
+		B[i * i] = 1
+		if i * i * i < 2000:
+			B[i * i * i] = 1
+		i += 1
+	"""
+
 	AN = list(map(int, input().split()))	# A1, A2, ... An
-
-	length = len(AN)
+	print(AN)
 	
-	total = hash(N, AN)
 
+	total = hash(N, AN)
 	print(total)
 	
 
