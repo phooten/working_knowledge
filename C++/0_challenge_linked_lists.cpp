70060021c7ef1a50d85e02bbca7873d4c4c13472
phooten <parkerhooten@gmail.com>
2020-01-12 16:06:43 -0600

    20%

--- C++/0_challenge_linked_lists.cpp
+++ C++/0_challenge_linked_lists.cpp
@@ -7,7 +7,11 @@
 // log
 //---------------------------------
 // [1/8/2020]
-//		- 
+//		- got foundational code set up
+// [1/12/2020]
+//		- my idea is to set up a linked list from 2D matrix. I'm not sure
+//		  how many elements in each list will be, nor how many groupings 
+//		  there will be. 
 //---------------------------------
 
 
@@ -21,6 +25,62 @@ using namespace std;
 
 #define	arrSize 2001
 
+
+//linked list set up
+//---------------------------------------
+
+// EXAMPLE 1 : Hacker Earth
+/*
+struct linkedList{
+	int data;
+	struct linkedList *next; // self-referencing pointer
+};
+
+typedef struct linkedList *node; //node is a point of data type struct linkedList
+
+node createNode(){
+	node temp;
+	temp = (node)malloc(sizeof(struct linkedList));
+	temp->next = Null;
+	return temp;
+}
+
+node addNoed(node head, int value){
+	node temp, p;
+	temp = createNode();
+	temp->data = value;
+	if(head == NULL){
+		head = temp;
+	} else {
+		p = head;
+		while(p->next != NULL){
+			p = p->next;
+		}
+		p->next = temp;
+	}
+	return head;
+}*/
+//---------------------------------------
+
+struct node{
+	int data;
+	node *next;
+};
+
+void moveArr(){
+	/*
+	n = new node;	// n points to newly created node
+	n->data = 1;	// to access the data part of node
+	temp = n;
+	head = n;
+
+	n = new node;	//
+	n->data = 2;
+	temp->next = n;
+	temp = t->next;
+	*/
+}
+
 int main(){
 /*
 	int num;
@@ -31,6 +91,7 @@ int main(){
 	int arr[arrSize];
 	int n;
 	int temp;
+		
 
 	// Gets input
 	scanf("%i", &n);
@@ -38,6 +99,8 @@ int main(){
 		scanf("%i", &temp);
 		arr[i] = temp;
 	}
+	
+	
 
 	// Prints out results
 	printf("\n\n");
