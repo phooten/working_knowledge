ddacdba4486563ad8ab6683f95fe42fb9fa4c0b8
phooten <parkerhooten@gmail.com>
2020-02-19 01:57:24 -0600

    Clean this all up this weekend

--- C++/1_pointerPractice_02192020.cpp
+++ C++/1_pointerPractice_02192020.cpp
@@ -4,14 +4,28 @@
 
 #include <stdio.h>
 
-void increment(int *v){
-	(*v)++;
+void update(int* a, int* b){
+	// a' = 4 + 5 = 9
+	printf("%p %d end 1\n", a, *a);	
+	*a = 4 + 5;	
+	printf("%p %d end 2\n", a, *a);
+	
+	printf("%p %d end 3\n", b, *b);
+	*b = 4 - 5;
+	if(*b < 0){
+		*b *= -1;
+	}
+	printf("%p %d end 4\n", b, *b);
+	// b' = |4 - 5| = 1
 }
 
 int main(){
-	int a;
-	scanf("%d",&a);
-	increment(&a);
-	printf("%d\n",a);
+	int a, b;
+	int* pa = &a;
+	int* pb = &b;
+
+	scanf("%d %d", &a, &b);
+	update(pa, pb);
+	//printf("%d %d\n",a, b);
 	return 0;
 }
