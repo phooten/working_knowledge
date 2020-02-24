e41210bab915332d31f772050a3e2826405ec802
phooten <parkerhooten@gmail.com>
2020-02-24 00:27:34 -0600

    update: 2.24.2020

---
+++ C++/2_linkedList_practice_02232020.cpp
@@ -0,0 +1,61 @@
+/*
+	Author:		Parker Hooten
+	Purpose:	
+	Language:	C++
+*/
+
+// Status: INCOMPLETE - [2/23/2020]
+
+// Log:
+//---------------------------------------------------------------------------
+// [2/23/2020]
+//		- I have a pretty good idea about the basics of linked list and was 
+// 		  able to create a small one. Everything created today was hardcoded
+//		  so the next step will be to try to implement an add/delete function.
+//		  MAKE SURE TO COMPILE WITH "g++ ..."
+// [2/24/2020]
+//		- 
+//---------------------------------------------------------------------------
+
+#include <iostream>
+#include <stdio.h>
+#include <cstddef>
+using namespace std;
+
+class node {
+	public:
+		int data;
+		node *next;
+};
+
+int main(){
+	node *head = NULL;
+	node *second = NULL;
+	node *third = NULL;
+
+	head = new node();
+	second = new node();
+	third = new node();
+
+	head->data = 1;
+	head->next = second;
+	
+	second->data = 2;
+	second->next = third;
+
+	third->data = 3;
+	third->next = NULL;
+
+	//printf("first |   data: %d  next %d\n", head->data, head->next);
+	//printf("first |   data: %d\n", head->data);
+	cout << "first |   data: " << head->data << "   next: " << head->next << endl;
+	cout << "second |  data: " << second->data << "   head: " << second << " next: " << second->next << endl;
+	cout << "third |   data: " << third->data << "   next: " << third << " next: " << third->next << endl;
+	return 0;
+}
+
+
+
+
+
+
