175e1e29adf9021af63a9e123c40ccacb05e8d64
phooten <parkerhooten@gmail.com>
2021-08-26 18:38:08 -0400

    organization and start of algorithm study

---
+++ C++/competative/algorithms/quicksort.cpp
@@ -0,0 +1,48 @@
+/*
+	Author:		Parker Hooten
+	Purpose:	Study of Quicksort
+	Language:	C++
+	Date: 		August 26, 2021
+*/
+
+// Status: INCOMPLETE - [x/x/xxxx]
+
+// Log:
+//-------------------------------------------------------
+// [x/x/xxxx]
+//		-
+//-------------------------------------------------------
+
+/*
+	Notes:
+		- Averages: O(n logn) 
+		- Worst Case: O(n^2)
+		- "divide and conquer algorithm"
+		- Recursivley perform: Bring PIVOT to where left of the pivot is smaller than the right, 
+			quick sort left, quick sort right
+		- 
+*/
+
+int quickSort(int data[], int begin, int end){
+	int pivot = data[0];
+	
+	return;
+}
+
+int main(){
+	int length = 100;
+	int arr[length];
+
+	// Generate random array values	
+	for(int i = 0; i < length; i++){
+		arr[i] = rand();
+	}
+
+	return;
+}
+
+
+
+
+
+
