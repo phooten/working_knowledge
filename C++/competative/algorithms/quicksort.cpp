9745c52d68fa674d4cb9035ebdf491faab4657d9
phooten <parkerhooten@gmail.com>
2021-08-27 15:39:50 -0400

    got basic algo down, haven't tested. Num gen works for 0-9999

--- C++/competative/algorithms/quicksort.cpp
+++ C++/competative/algorithms/quicksort.cpp
@@ -23,22 +23,57 @@
 		- 
 */
 
-int quickSort(int data[], int begin, int end){
-	int pivot = data[0];
-	
-	return;
-}
+#include <iostream>
+using namespace std;
 
-int main(){
-	int length = 100;
-	int arr[length];
-
-	// Generate random array values	
-	for(int i = 0; i < length; i++){
-		arr[i] = rand();
+void printArr(int arr[], int len){
+	for(int i = 0; i < len; i++){
+		cout << "a[" << i <<"]: " << arr[i] << endl; 
 	}
 
 	return;
+}
+
+
+/*
+int quickSort(int arr[], int start, int end){
+	if(start < end){
+		int pivot;
+		// pivot = partition(arr, start, end);
+		quickSort(arr, start, pivot - 1);
+		quickSort(arr, pivot + 1, end);
+	}
+}
+*/
+/*
+int partition(int arr[], int start, int end){
+	len = len(arr);
+	int pivot = arr[end];
+	int i = begin - 1;
+
+	for(int j = begin; j <= end - 1; j++){
+		if(arr[j] <= pivot){
+			i++;
+			swap(&arr[i], &arr[end]);
+		}
+	}
+	swap(&arr[i + 1], &arr[end]);
+
+	return (i + 1);
+}
+*/
+int main(){
+	int length = 100;
+	int data[length];
+
+	// Generate random array values	
+	for(int i = 0; i < length; i++){
+		data[i] = rand() % 10000;
+	}
+
+	printArr(data, length);
+
+	return 0;
 }
 
 
