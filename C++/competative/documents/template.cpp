e0120bb291a7d204e419a8e3f94f8bf01b42911c
phooten <parkerhooten@gmail.com>
2021-09-05 20:34:56 -0400

    updated template to be alittle more standard

--- C++/competative/documents/template.cpp
+++ C++/competative/documents/template.cpp
@@ -1,35 +1,33 @@
 /*
-	Author:
-	Purpose:
+	Below is a template of a formatting structure I found on: 
+		-> edparrish.net/common/cppdoc.html#curlybrace
 */
 
-// Status: INCOMPLETE - [x/x/xxxx]
-
-/*
-	Log:
-	-------------------------------------------------------
-	[x/x/xxxx]
-		- 
-
-	-------------------------------------------------------
+/**
+    Overview: Description of the file
+    @file main.cpp
+    @author Parker Hooten
+    @version 1.1 5/9/2021 [ D/M/Y ]
 */
 
-
-/*
-	Function Name
-		Purpose:
-			answer
-		Arguments:
-			arg1 -> 
-			arg2 -> 
+/**
+    Description of the function
+	@param input the input into the function.
+    @return what is being returned.
 */
+int placeHolder();
+
+// Controls operation of the program.
 int main(){
 
 	return 0;
 }
 
+// Description of the function 
+int placeHolder(){
 
-
+	return 0;
+}
 
 
 
