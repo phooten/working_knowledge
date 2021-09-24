7b66af5839841740b6fdd77f8ad20e6ab1ad7f1a
phooten <parkerhooten@gmail.com>
2021-09-23 23:10:58 -0400

    start of dijkstra's algo

---
+++ C++/practice/algorithms/Dijakstra/main.cpp
@@ -0,0 +1,49 @@
+/*
+    Notes:
+        - Cannot be used if edges have negative weight
+        - Versions: 
+            - "Lazy" Dijkstra's
+            - 
+
+    Algorithm:
+        - Initialize all distances to get to each node from node A to infinity
+        - Assign start node to be 0
+        - Visit all nodes directly attached to node 0, and reassign shortest distances IF they are shorter
+        - Visit the next most promising node in the priority queue
+
+    References: 
+        - Graph Data Structure 4. Dijkstra’s Shortest Path Algorithm: https://www.youtube.com/watch?v=pVfj6mxhdMw
+        - 
+*/
+
+/**
+    Overview: Description of the file
+    @file       main.cpp
+    @author     Parker Hooten
+    @version    1.1 
+    @date       9/23/2021 [ D/M/Y ]
+*/
+
+/**
+    Description of the function
+	@param input the input into the function.
+    @return what is being returned.
+
+
+*/
+
+class VertexList{
+    int node;
+    int shortestDist;   // shortest distance from x
+    int prevVertex;
+};
+
+// Controls operation of the program.
+int main() {
+
+
+
+    return 0;
+}
+
+
