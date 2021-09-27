75c5ace4d9eadfd1ab1709088b03cc8b691b7e65
phooten <parkerhooten@gmail.com>
2021-09-26 23:48:24 -0400

    .

--- C++/practice/datastructures/graph/main.cpp
+++ C++/practice/datastructures/graph/main.cpp
@@ -1,30 +1,77 @@
+/*
+    Notes:
+        - Cannot be used if edges have negative weight
+        - Versions: 
+            - "Lazy" Dijkstra's
+            - 
+
+    Algorithm:
+        - Initialize all distances to get to each node from node A to infinity
+        - Assign start node to be 0
+        - Visit all nodes directly attached to node 0, and reassign shortest distances IF they are shorter
+        - Visit the next most promising node in the priority queue
+
+    References: 
+        - Graph Data Structure 4. Dijkstra’s Shortest Path Algorithm: https://www.youtube.com/watch?v=pVfj6mxhdMw
+        - 
+*/
+
+/**
+    Overview: Description of the file
+    @file       main.cpp
+    @author     Parker Hooten
+    @version    1.1 
+    @date       9/23/2021 [ D/M/Y ]
+*/
+
+/**
+    Description of the function
+	@param input the input into the function.
+    @return what is being returned.
 
 
+*/
+
+//#include<stdio.h>
 #include <iostream>
 #include <string>
 
-class GraphList{
+const unsigned short tmp = 10;
+
+class Vertex
+{
 public:
-    unsigned int nodeLength;
-    unsigned int graph[nodeLength][nodeLength];
+    std::string name;
+    bool visited;
 
-
-    GraphList(){
-        nodeLength = 0;
+    Vertex(std::string name)
+    {
+        name = "";
+        visited = false;
     }
 
-    GraphList(unsigned int input){
-        nodeLength = input;    
-/*
-        for(int i = 0; i < nodeLength; i++){
-            for(int j = 0; j < nodeLength; j++){
-                unsigned int graph[i][j] = 0;
-            }
-        }
-*/
-    }
+private:
 };
 
-int main(){
-       GraphList Sample(100);
+class Graph
+{
+public:
+    Graph()
+    {
+        const unsigned int totalVertices;
+        Vertex vertices[totalVertices];
+        double adjacentcyMatrix[tmp][tmp];
+    }
+
+private:
+    const unsigned int totalVertices;
+    Vertex vertices[tmp];
+    double adjacentcyMatrix[tmp][tmp];
+};
+
+// Controls operation of the program.
+int main()
+{
+
+    return 0;
 }
