a873a7748bbd2431a571c116f8aeb9b9da70f809
phooten <parkerhooten@gmail.com>
2021-10-03 20:42:01 -0400

    folder specifically meant to learn about moving semantics. There seems to be quite a bit going into this.

---
+++ C++/knowledgeLibrary/types - INCOMPLETE/moveSemantics/README.txt
@@ -0,0 +1,10 @@
+This folder is dedicated to learning about Move Semantics. Most of the 
+examples were written while following "the Cherno" youtube channel. This code is
+meant to be a reference. See "References" for specific videos.  
+
+
+References:
+    "Move Semantics in C++" - https://www.youtube.com/watch?v=ehMg6zvXuMY
+    "CppCon 2019: Klaus Iglberger “Back to Basics: Move Semantics (part 1 of 2)”"
+        - https://www.youtube.com/watch?v=St0MNEU5b0o
+    
---
+++ C++/knowledgeLibrary/types - INCOMPLETE/moveSemantics/main.cpp
@@ -0,0 +1,105 @@
+/*
+    Date:           10-3-2021
+    File:           main.cpp
+    Version:        1.0
+    Status:         COMPLETE
+    
+    Contributers:   Parker Hooten
+                    The Cherno (see Credits)
+
+    Purpose:        To learn about moving semantics and how we can optimize our
+                    code through moving rather than copying. Below shows an 
+                    extremely basic string class and how we might go about 
+                    moving it over to be accessed by a different class.  
+
+    Credits:
+        The Cherno:
+            -   This code was written by this youtube channel. It was written
+                almost completely based on the following link, with the purposes
+                of learning about arrays, and better programming practice.
+                Link:  https://www.youtube.com/watch?v=ehMg6zvXuMY
+*/
+
+#include<iostream>
+#include<cstring>
+
+class String{
+public:
+    String() = default;
+    String(const char* string){
+        printf("Created!\n");
+        m_Size = strlen(string);
+        m_Data = new char[m_Size];
+        memcpy(m_Data, string, m_Size);
+    }
+
+    /*
+        Copy Constructor
+        -   Takes in lvalue reference
+    */
+    String(const String& other){
+        printf("Copied!\n");
+        m_Size = other.m_Size;
+        m_Data = new char[m_Size];
+        memcpy(m_Data, other.m_Data, m_Size);
+    }
+
+    /*
+        Move Constructor
+        -   Takes in rvalue reference
+    */
+    String(String&& other) noexcept {
+        printf("Moved!\n");
+        m_Size = other.m_Size;
+        m_Data = other.m_Data;
+
+        other.m_Size = 0;
+        other.m_Data = nullptr;
+    }
+
+
+    ~String(){
+        printf("Destroyed\n");
+        delete m_Data;
+    }
+
+    void Print(){
+        for(uint32_t i = 0; i < m_Size; i++){
+            printf("%c", m_Data[i]);
+        }
+        printf("\n");
+    }
+
+private:
+    char* m_Data;
+    uint32_t m_Size;
+};
+
+
+
+class Entity{
+    public:
+        Entity(const String& name)
+            : m_Name(name) {}
+        
+        Entity(String&& name)
+            : m_Name((String&&)name) {}
+       
+        void PrintName(){
+            m_Name.Print();
+        }
+
+    private:
+        String m_Name;
+};
+
+
+
+int main(){
+    Entity entity(String("Cherno"));
+    entity.PrintName();
+
+    return 0;
+}
+
+
