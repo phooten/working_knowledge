ddb2ff44ba0a0d4687f179802009f831953a33a2
phooten <parkerhooten@gmail.com>
2021-10-05 23:20:34 -0400

    added laptop version bc Desktop is so much faster, especially after bootup. Got tired of having to adjust things

--- scripts/braveMiner/2_batMiner-Laptop-wsl.sh
+++ scripts/braveMiner/2_batMiner-Laptop-wsl.sh
@@ -5,13 +5,21 @@
 # tasklist.exe /FI "WINDOWTITLE eq New Tab - Brave" /fo list | grep PID
 
 
+
+# Sets up the display. I ALWAYS have to do this on laptop
+export DISPLAY="`grep nameserver /etc/resolv.conf | sed 's/nameserver //'`:0"
+
+sleep 2
+
 # opens up a new terminal and runs brave
 gnome-terminal -- ./dummyTerminal.sh
+
+sleep 7
 
 pid=$(pgrep -f gnome-terminal)
 
 count=20    
-time=0.5
+time=1
 for i in $( seq 0 $count)
 do 
     echo $i
---
+++ scripts/braveMiner/B_batMiner-Desktop-wsl.sh
@@ -0,0 +1,25 @@
+# Opens tasklist and filters out brave.exe. 
+# /V shows verbose information
+# tasklist.exe /FI "IMAGENAME eq brave.exe" /FI "STATUS eq RUNNING" /V
+# tasklist.exe /FI "WINDOWTITLE eq New Tab - Brave" /V /fo csv
+# tasklist.exe /FI "WINDOWTITLE eq New Tab - Brave" /fo list | grep PID
+
+# opens up a new terminal and runs brave
+gnome-terminal -- ./dummyTerminal.sh
+
+pid=$(pgrep -f gnome-terminal)
+
+count=20    
+time=0.5
+for i in $( seq 0 $count)
+do 
+    echo $i
+    sleep $time
+    xdotool key CTRL+R    
+done
+
+#kill "$pid"
+
+
+
+# ./~/General-Practice/scripts/batMiner_wsl.sh
