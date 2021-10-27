86b9313379ca48ba602bdc42e9446d945f1bd869
phooten <parkerhooten@gmail.com>
2021-10-26 23:41:54 -0400

    threading in python

---
+++ PYTHON/knowledgeLibrary/other/main.py
@@ -0,0 +1,36 @@
+# https://www.youtube.com/watch?v=IEEhzQoKtQU
+
+import threading
+import time
+
+start = time.perf_counter()
+
+def do_something():
+    print('sleeping 1 second...')
+    time.sleep(1)
+    print('done sleeping.')
+
+#  Note 2:
+# t1 = threading.Thread(target=do_something)
+# t2 = threading.Thread(target=do_something)
+
+# # note 1: 
+# # without the .join(), the threads will run here, then finish the script while these two threads are still waiting
+# t1.start()
+# t2.start()
+
+# t1.join()
+# t2.join()
+
+threads = []
+for _ in range (10):                            # _ is a throw away variable
+    t = threading.Thread(target=do_something)
+    t.start()
+    threads.append(t)
+
+for thread in threads:
+    thread.join()
+
+finish = time.perf_counter()
+
+print(f'finished in {round(finish - start, 2)} second(s)')
