d3d7a8fa8b5be847493b8ae9cf77ed25015c3810
phooten <parkerhooten@gmail.com>
2023-05-19 23:02:53 -0600

    want to start a couple of new programs.

---
+++ movie_list.py
@@ -0,0 +1,24 @@
+'''This script is meant to record the date a movie was watched'''
+
+
+movie_excel_sheet="movie_list.xml"
+
+class Movie:
+    title=''
+    release_date=''
+    watch_date=''
+    rating=''
+
+
+def GetUserInput():
+    
+    return:
+
+
+def main():
+
+    return
+
+if __name__=="__main__":
+    main()
+
