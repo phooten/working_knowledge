11dd7d9a2c01d0930336c7f4f0ce6c611d6f9050
Parker Hooten <phooten@g.clemson.edu>
2023-09-10 22:32:42 -0600

    adding new tool and notes

---
+++ scripts/bin/check_formatting.sh
@@ -0,0 +1,10 @@
+# The purpose of this script is to check different files and make sure they abide
+# by the following rules, regaurdless of the language:
+#   - Less than 120 character per line
+#   - Header with the following details:
+#       - Title
+#       - Description
+#       - Usage
+#   - No Tabs, only spaces ( exception of make file )
+#   - No "TODO" comments, regaurdless of case
+
